aa6a658a25fb25932433c69a22157acd095aed36
Alexandr <alexandr.semenovv.1996@gmail.com>
2022-12-16 01:37:35 +0300

    Добавлена пагинация

--- scraping/urls.py
+++ scraping/urls.py
@@ -4,5 +4,6 @@ from scraping import views
 
 
 urlpatterns = [
-    path('home/', views.home_view),
+    path('home/', views.home_view, name='home'),
+    path('list/', views.list_view, name='list'),
 ]
--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 from .forms import FindForm
@@ -9,16 +10,37 @@ from .models import *
 
 def home_view(request):
     form = FindForm()
+
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    queryset = []
-    _filter = {}
+    context = {'city': city, 'language': language, 'form': form}
+
     if city or language:
+        _filter = {}
         if city:
             _filter['city__slug'] = city
         if language:
             _filter['language__slug'] = language
 
-    queryset = Vacancy.objects.filter(**_filter)
+        queryset = Vacancy.objects.filter(**_filter)
+        paginator = Paginator(queryset, 25)
 
-    return render(request, 'scraping/home.html', {'object_list': queryset, 'form': form})
+        page_number = request.GET.get('page', 1)
+
+        try:
+            page_obj = paginator.get_page(page_number)
+        except PageNotAnInteger:
+            # Если передан тип данных не int
+            page_obj = paginator.get_page(1)
+        except EmptyPage:
+            # Если передана пустая страница
+            page_obj = paginator.get_page(paginator.num_pages)
+
+        context['object_list'] = page_obj
+
+    return render(request, 'scraping/list_view.html', context)
